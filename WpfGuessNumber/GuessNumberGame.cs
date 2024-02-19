a906874b941764adae4111c922c846817d46f197
Elitsa Georgeva <Elitsa Georgeva@DESKTOP-8D1ST4F>
2024-02-19 21:55:32 +0200

    Add project files.

---
+++ WpfGuessNumber/GuessNumberGame.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfGuessNumber
+{
+    internal class GuessNumberGame
+    {
+        public enum Difficulty
+        {
+            Easy,
+            Medium,
+            Hard
+        }
+
+        public const int MinNumber = 1;
+        public const int MaxNumber = 100;
+
+        private int targetNumber;
+        private int attemptsLeft;
+        private int lastGuess;
+
+        public int TargetNumber => targetNumber;
+        public int AttemptsLeft => attemptsLeft;
+
+        public void StartNewGame(Difficulty difficulty)
+        {
+            switch (difficulty)
+            {
+                case Difficulty.Easy:
+                    attemptsLeft = 7;
+                    break;
+                case Difficulty.Medium:
+                    attemptsLeft = 4;
+                    break;
+                case Difficulty.Hard:
+                    attemptsLeft = 2;
+                    break;
+            }
+
+            Random random = new Random();
+            targetNumber = random.Next(MinNumber, MaxNumber+1);
+            lastGuess = -1; 
+        }
+
+        public enum GameResult
+        {
+            Correct,
+            High,
+            Low
+        }
+
+        public GameResult CheckGuess(int userGuess)
+        {
+            if (attemptsLeft > 0)
+            {
+                if (userGuess == targetNumber)
+                {
+                    return GameResult.Correct;
+                }
+                else if (userGuess > targetNumber)
+                {
+                    attemptsLeft--; 
+                    lastGuess = userGuess;
+                    return GameResult.High;
+                }
+                else
+                {
+                    attemptsLeft--; 
+                    lastGuess = userGuess;
+                    return GameResult.Low;
+                }
+            }
+            else
+            {
+                return GameResult.Correct; 
+            }
+        }
+
+        public string CheckGuessDirectionMessage()
+        {
+            if (attemptsLeft > 0 && lastGuess != -1)
+            {
+                if (lastGuess < targetNumber)
+                {
+                    return "Go higher!";
+                }
+                else
+                {
+                    return "Go lower!";
+                }
+            }
+
+            return string.Empty; 
+        }
+    }
+}
---
+++ WpfGuessNumber/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfGuessNumber
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GuessNumberGame game;
+        public MainWindow()
+        {
+            InitializeComponent();
+            game = new GuessNumberGame();
+        }
+
+        private void EasyClick(object sender, RoutedEventArgs e)
+        {
+            StartGame(GuessNumberGame.Difficulty.Easy);
+        }
+
+        private void MediumClick(object sender, RoutedEventArgs e)
+        {
+            StartGame(GuessNumberGame.Difficulty.Medium);
+        }
+
+        private void HardClick(object sender, RoutedEventArgs e)
+        {
+            StartGame(GuessNumberGame.Difficulty.Hard);
+        }
+        private void StartGame(GuessNumberGame.Difficulty difficulty)
+        {
+            game.StartNewGame(difficulty);
+            DisplayMessage($"Guess a number between {GuessNumberGame.MinNumber} and {GuessNumberGame.MaxNumber}");
+
+            
+            CheckGuessButton.IsEnabled = true;
+            UserInputTextBox.IsEnabled = true;
+        }
+
+        private void CheckGuessButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (game.AttemptsLeft > 0)
+            {
+                if (int.TryParse(UserInputTextBox.Text, out int userGuess))
+                {
+                    GuessNumberGame.GameResult result = game.CheckGuess(userGuess);
+
+                    if (result == GuessNumberGame.GameResult.Correct)
+                    {
+                        DisplayMessage($"Congratulations! You guessed the number {game.TargetNumber}! Play again?");
+                        
+                        CheckGuessButton.IsEnabled = false;
+                        UserInputTextBox.IsEnabled = false;
+                    }
+                    else
+                    {
+                        DisplayMessage(result == GuessNumberGame.GameResult.High
+                            ? "Too high. " + game.CheckGuessDirectionMessage()
+                            : "Too low. " + game.CheckGuessDirectionMessage());
+                    }
+                }
+                else
+                {
+                    DisplayMessage("Invalid input. Please enter a valid number.");
+                }
+            }
+            else
+            {
+                DisplayMessage($"Sorry, you ran out of attempts. The correct number was {game.TargetNumber}. Play again?");
+                
+                CheckGuessButton.IsEnabled = false;
+                UserInputTextBox.IsEnabled = false;
+            }
+        }
+
+        private void DisplayMessage(string message)
+        {
+            MessageTextBlock.Text = message;
+        }
+    }
+}
+
